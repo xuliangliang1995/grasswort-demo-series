23e3449d81c38df6de49c5430f4be31777299c78
xuliang <grasswort@email.com>
2021-04-06 16:15:45 +0800

    first commit

---
+++ io/src/main/java/GenericDemo.java
@@ -0,0 +1,34 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Created on 2021/4/6.
+ *
+ * @author xuliangliang
+ */
+public class GenericDemo {
+
+    public static void main(String[] args) {
+        ByteArrayInputStream is = new ByteArrayInputStream("徐靓靓".getBytes(StandardCharsets.UTF_8));
+
+        int size = is.available();
+        byte[] bytes = new byte[size];
+        try {
+            int readBytes = is.read(bytes);
+            assert readBytes == size;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String readContent = new String(bytes, StandardCharsets.UTF_8);
+        System.out.println(readContent);
+    }
+}
