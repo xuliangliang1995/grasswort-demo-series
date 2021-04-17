bea2862559a5458e2afca63f35c5efb2a962e12f
许良良 <grasswort@qq.com>
2021-04-17 15:57:48 +0800

    hash demo

---
+++ redis/src/main/java/jedis/operation/HashOperationDemo.java
@@ -0,0 +1,42 @@
+package jedis.operation;
+
+import jedis.JedisSentinelPoolFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisSentinelPool;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 🌺xuliangliang🌺
+ * @Description
+ * @Date 2021/4/17
+ */
+public class HashOperationDemo {
+
+    public static void main(String[] args) {
+        JedisSentinelPool sentinelPool = JedisSentinelPoolFactory.sentinelPool();
+        Jedis jedis = sentinelPool.getResource();
+
+        String hashKey = "hash";
+        jedis.del(hashKey);
+
+        Map<String, String> fields = new HashMap<>();
+        fields.put("name", "jerry");
+        fields.put("age", "8");
+        fields.put("id", "1");
+        fields.put("email", "grasswort@qq.com");
+
+        Long rs = jedis.hset(hashKey, fields);
+        System.out.println(rs);
+
+        jedis.hgetAll(hashKey).forEach((key, value) -> System.out.printf("%s : %s\n", key, value));
+
+        jedis.hincrBy(hashKey, "age", 1);
+        String age = jedis.hget(hashKey, "age");
+        System.out.println(age);
+
+        sentinelPool.destroy();
+        sentinelPool.close();
+    }
+}
