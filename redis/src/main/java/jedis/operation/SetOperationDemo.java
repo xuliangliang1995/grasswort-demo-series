e7c3740874badeb4dbe5bab82dd9ecba7b6a508f
许良良 <grasswort@qq.com>
2021-04-17 16:05:43 +0800

    set demo

---
+++ redis/src/main/java/jedis/operation/SetOperationDemo.java
@@ -0,0 +1,29 @@
+package jedis.operation;
+
+import jedis.JedisSentinelPoolFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisSentinelPool;
+
+/**
+ * @author 🌺xuliangliang🌺
+ * @Description
+ * @Date 2021/4/17
+ */
+public class SetOperationDemo {
+
+    public static void main(String[] args) {
+        JedisSentinelPool sentinelPool = JedisSentinelPoolFactory.sentinelPool();
+        Jedis jedis = sentinelPool.getResource();
+
+        String setKey = "set";
+        jedis.sadd(setKey, "tom");
+        jedis.sadd(setKey, "tom");
+        jedis.sadd(setKey, "jerry");
+
+        jedis.smembers(setKey)
+                .forEach(System.out::println);
+
+        sentinelPool.destroy();
+        sentinelPool.close();
+    }
+}
