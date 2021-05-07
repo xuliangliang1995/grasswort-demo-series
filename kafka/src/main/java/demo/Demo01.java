75a230d6293cc09346debf7a05d37d11527f211a
xuliang <grasswort@qq.com>
2021-05-07 16:51:09 +0800

    kafka demo

--- kafka/src/main/java/demo/Demo01.java
+++ kafka/src/main/java/demo/Demo01.java
@@ -47,6 +47,7 @@ public class Demo01 {
         // kafka 持久化数据的 MQ， 不会加工数据， 双方需要约定编解码
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "0");
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
@@ -54,7 +55,7 @@ public class Demo01 {
 
         Random random = new Random();
         while (true) {
-            TimeUnit.SECONDS.sleep(random.nextInt(5));
+            TimeUnit.SECONDS.sleep(random.nextInt(3));
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
                     String key = "item" + j;
@@ -78,7 +79,7 @@ public class Demo01 {
 
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_001");
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // latest
-        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true"); //自动提交时，异步提交(Consumer 挂了)，很容易丢数据和重复消费
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); //自动提交时，异步提交(Consumer 挂了)，很容易丢数据和重复消费
         // properties.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, ""); 5s
         // properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "");
 
@@ -87,8 +88,11 @@ public class Demo01 {
 
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(5)); // 0 ~ 多条
+            // 1. 按每条记录（最傻）
+            // 2. 按分区粒度（每个分区有自己对应的 offset， 可以多线程处理）
+            // 3. 按当前 poll 批次同步提交（复杂）
 
-            System.out.println("poll " + records.count() + "条数据。");
+            System.out.println("poll " + records.count() + " 条数据。");
             // 以下代码优化很重要
             Iterator<ConsumerRecord<String, String>> iterator = records.iterator();
 
@@ -98,6 +102,8 @@ public class Demo01 {
                 long offset = record.offset();
                 System.out.println("key:" + record.key() + ", value:" + record.value() + ", partition:" + partition + ", offset:" + offset);
             }
+            // 按批次提交
+            consumer.commitAsync();
         }
 
     }
