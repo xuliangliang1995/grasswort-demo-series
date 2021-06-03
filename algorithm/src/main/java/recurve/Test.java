50540940d587dcf94b6a3f63cfbeb007fb19adb6
xuliang <grasswort@qq.com>
2021-06-03 17:22:09 +0800

    hanoi tower demo

---
+++ algorithm/src/main/java/recurve/Test.java
@@ -0,0 +1,15 @@
+package recurve;
+
+/**
+ * @author xuliangliang
+ * @Description
+ * @Date 2021/6/3
+ */
+public class Test {
+
+
+
+    private static void move(int size, String from, String to, String other) {
+
+    }
+}
---
+++ algorithm/src/main/java/recurve/hanoi/HanoiTowerProblem.java
@@ -0,0 +1,9 @@
+package recurve.hanoi;
+
+/**
+ * @author xuliangliang
+ * @Description 汉诺塔问题
+ * @Date 2021/6/3
+ */
+public class HanoiTowerProblem {
+}
