19f3d539e5eafcefab85cd179a3a0d686d07bbf8
许良良 <grasswort@qq.com>
2021-06-20 21:27:51 +0800

    socket demo

---
+++ io/src/main/java/io/socket/ClientDemo.java
@@ -0,0 +1,54 @@
+package io.socket;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.Scanner;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @author 🌺xuliangliang🌺
+ * @Description
+ * @Date 2021/6/20
+ */
+public class ClientDemo {
+
+    public static void main(String[] args) throws IOException {
+        // socket()
+        Socket socket = new Socket();
+        socket.setSendBufferSize(20);
+        socket.setTcpNoDelay(true);
+
+        // bind()
+        SocketAddress socketAddress = new InetSocketAddress("localhost", 8080);
+        socket.bind(socketAddress);
+
+        // connect()
+        SocketAddress serverAddress = new InetSocketAddress("localhost", 10000);
+        socket.connect(serverAddress);
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String writeData = sc.nextLine();
+            bufferedWriter.write(writeData);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            String readData = bufferedReader.readLine();
+            System.out.println("recv : " + readData);
+
+            if ("bye".equals(readData)) {
+                socket.close();
+                break;
+            }
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+
+    }
+}
---
+++ io/src/main/java/io/socket/ServerDemo.java
@@ -0,0 +1,85 @@
+package io.socket;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+/**
+ * @author 🌺xuliangliang🌺
+ * @Description
+ * @Date 2021/6/20
+ */
+public class ServerDemo {
+
+    public static void main(String[] args) throws IOException {
+        // socket()
+        ServerSocket socket = new ServerSocket();
+
+        // bind() and listen()
+        SocketAddress serverAddress = new InetSocketAddress("localhost", 10000);
+        socket.bind(serverAddress, 3);
+
+        System.out.println("服务已启动.");
+
+        // accept() block
+        for (;;) {
+            Socket clientSocket = socket.accept();
+            new ServerThread(clientSocket).start();
+        }
+
+    }
+
+    static class ServerThread extends Thread {
+
+        private final Socket clientSocket;
+
+        ServerThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            String clientInfo = clientSocket.getInetAddress().getHostAddress() + ":" + clientSocket.getPort();
+            System.out.println(clientInfo + "已连接.");
+            BufferedReader bufferedReader = null;
+            BufferedWriter bufferedWriter = null;
+            try {
+                bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                while (true) {
+                    String readData = bufferedReader.readLine();
+                    System.out.println("recv : " + readData);
+                    bufferedWriter.write(readData);
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+
+                    if ("bye".equals(readData)) {
+                        // bye bye
+                        System.out.println(clientInfo + "已断开连接");
+                        clientSocket.close();
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (bufferedReader != null) {
+                        bufferedReader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    if (bufferedWriter != null) {
+                        bufferedWriter.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
