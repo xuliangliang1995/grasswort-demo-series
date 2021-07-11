f7151751b1f1cccb07abad17c7d70708c1bf02bf
许良良 <grasswort@qq.com>
2021-07-11 10:24:09 +0800

    Quick Sort

--- algorithm/src/main/java/sort/QuickSort.java
+++ algorithm/src/main/java/sort/QuickSort.java
@@ -2,12 +2,17 @@ package sort;
 
 import sort.util.IntArrayUtil;
 
+import java.util.Random;
+
 /**
  * @author xuliangliang
  * @Description 快速排序 O(N * logN)
  * @Date 2020/7/18
  */
 public class QuickSort implements IMathArraySort {
+
+
+    private Random random = new Random();
     /**
      * 数组排序, 等同于 Arrays.sort(int[] array)
      *
@@ -20,32 +25,32 @@ public class QuickSort implements IMathArraySort {
         quickSort(array, L, R);
     }
 
+
     /**
      * quick sort
-     * @param array
+     * @param arr
      * @param L
      * @param R
      */
-    private void quickSort(int[] array, int L, int R) {
-        IntArrayUtil.swap(array, (int)(Math.random() * (R - L + 1)) + L, R);
-        int mum = array[R];
-        int less = L - 1;
-        int more = R + 1;
+    private void quickSort(int[] arr, int L, int R) {
+        if (L >= R) {
+            return;
+        }
+        IntArrayUtil.swap(arr, R, random.nextInt(R - L + 1) + L);
+        int number = arr[R];
+        int lessCursor = L - 1;
+        int rightCursor = R + 1;
         int i = L;
-        while (i < more) {
-            if (array[i] < mum) {
-                IntArrayUtil.swap(array, i++, ++less);
-            } else if (array[i] > mum) {
-                IntArrayUtil.swap(array, i, more--);
-            } else {
-                IntArrayUtil.swap(array, i++, less + 1);
+        while (i < rightCursor) {
+            if (arr[i] < number) {
+                IntArrayUtil.swap(arr, i, ++lessCursor);
+            } else if (arr[i] > number) {
+                IntArrayUtil.swap(arr, i, --rightCursor);
+                i--;
             }
+            i++;
         }
-        if (L < less) {
-            quickSort(array, L, less);
-        }
-        if (more < R) {
-            quickSort(array, more, R);
-        }
+        quickSort(arr, L, lessCursor);
+        quickSort(arr, rightCursor, R);
     }
 }
