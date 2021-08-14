382cf92863ff572ad4093900e1776bcc6f8a24c7
xuliang <grasswort@qq.com>
2021-08-14 22:02:59 +0800

    判断链表是否有环

---
+++ algorithm/src/main/java/codetop/LinkedListHasCycle.java
@@ -0,0 +1,56 @@
+package codetop;
+
+/**
+ * @author xuliangliang
+ * @Description 环形链表
+ * @Date 2021/8/14
+ */
+public class LinkedListHasCycle {
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node3;
+        System.out.println(new LinkedListHasCycle().hasCycle(node1));
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slowCursorNode = head;
+        ListNode quickCursorNode = head;
+        while (slowCursorNode != null && quickCursorNode != null) {
+            slowCursorNode = slowTraverse(slowCursorNode);
+            quickCursorNode = quickTraverse(quickCursorNode);
+            if (slowCursorNode == quickCursorNode) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private ListNode quickTraverse(ListNode node) {
+        return node != null
+                ? (node.next != null ? node.next.next : null)
+                : null;
+    }
+
+    private ListNode slowTraverse(ListNode node) {
+        return node != null ? node.next : null;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
